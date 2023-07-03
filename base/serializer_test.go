e3b710151d1c0b39ae116a9063a7ecd7fbadb23e
wale <2064737314@qq.com>
2023-07-03 23:52:16 +0800

    add ed25519 tests

--- base/serializer_test.go
+++ base/serializer_test.go
@@ -1,8 +1,6 @@
 package base
 
 import (
-	"cpk-algs/base/edwards25519"
-	"crypto/rand"
 	"testing"
 )
 
@@ -73,50 +71,6 @@ func TestSerializer_WriteInt32(t *testing.T) {
 	}
 	if str != "23456" {
 		t.Error("bad deserializer str:{}", str)
-		return
-	}
-}
-
-func TestSerializer_WriteSerializable(t *testing.T) {
-	var serializer Serializer
-	var randomBytes [64]byte
-	_, err := rand.Read(randomBytes[:])
-	if err != nil {
-		panic(err)
-	}
-	randomScalar := (&edwards25519.Scalar{}).SetUniformBytes(randomBytes[:])
-	var randomPriv Ed25519Scala
-	randomPriv.Scalar = randomScalar
-	serializer.WriteSerializable(&randomPriv)
-
-	randomPoint := (&edwards25519.Point{}).ScalarBaseMult(randomScalar)
-	var randomPub Ed25519Point
-	randomPub.Point = randomPoint
-	serializer.WriteSerializable(&randomPub)
-
-	deserializer, err := NewDeserializer(serializer)
-	if err != nil {
-		t.Error(err)
-		return
-	}
-
-	var randomPriv2 Ed25519Scala
-	_, err = deserializer.ReadSerializable(&randomPriv2)
-	if err != nil {
-		return
-	}
-	if randomScalar.Equal(randomPriv2.Scalar) != 1 {
-		t.Error("bad deserializer scala:{}", randomPriv2.Scalar)
-		return
-	}
-
-	var randomPub2 Ed25519Point
-	_, err = deserializer.ReadSerializable(&randomPub2)
-	if err != nil {
-		return
-	}
-	if randomPub.Equal(randomPub2.Point) != 1 {
-		t.Error("bad deserializer point:{}", randomPub2.Point)
 		return
 	}
 }
